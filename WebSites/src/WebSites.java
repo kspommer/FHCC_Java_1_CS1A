18c841982f406757ac0e0540e07713713e1bb441
Susan Pommer <kspommer@gmail.com>
2018-07-29 11:03:47 -0700

    FHCC Java 1 CS1A - Assignment Submission 1

---
+++ WebSites/src/WebSites.java
@@ -0,0 +1,45 @@
+/*
+ * Class: CS 1A
+ * Description: Week 1 Assignment:  This program prints a list of some of my favorite websites
+ * Due date: 1/18/2017
+ * Name: Susan Pommer
+ * File name: WebSites.java
+ */ 
+
+public class WebSites 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("These are some of my favorite websites:");
+		System.out.println();
+		System.out.println("American Kennel Club:");
+		System.out.println("http://www.akc.org/");
+		System.out.println();
+		System.out.println("Food Network:");
+		System.out.println("http://www.foodnetwork.com/");
+		System.out.println();
+		System.out.println("The Green Bay Packers:");
+		System.out.println("http://www.packers.com/");
+		System.out.println();
+		System.out.println("Cotogna:");
+		System.out.println("http://cotognasf.com/");
+		System.out.println();
+	}
+}
+
+/* -------------------- Sample Run ---------------------------
+*These are some of my favorite websites:
+*
+*American Kennel Club:
+*http://www.akc.org/
+*
+*Food Network:
+*http://www.foodnetwork.com/
+*
+*The Green Bay Packers:
+*http://www.packers.com/
+*
+*Cotogna:
+*http://cotognasf.com/
+*
+* ----------------------------------------------------------- */
